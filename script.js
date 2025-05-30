85d77c28f96b1e732201d7de418d9786dc0d38d7
PythonAnshil <anshillamba.py.58@gmail.com>
2025-05-30 13:15:22 -0400

    Maek some change and organise

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function toggleNav() {
-    document.getElementById('navbar').classList.toggle('open');
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('open');
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    var typed = new Typed('#element', {
-        strings: ['Web Developer', 'Data Analyst', 'Programmer'],
-        typeSpeed: 50,
-        backSpeed: 25,
-        loop: true
-    });
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Data Analyst', 'Programmer'],
+    typeSpeed: 50,
+    backSpeed: 25,
+    loop: true
 });
